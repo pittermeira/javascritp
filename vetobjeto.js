255283819a8ef1cb71eb6b9568ab754f4a6dfc17
PITTER MEIRA DE OLIVEIRA CAMARGO <21680@unifacef.edu.br>
2019-08-21 21:45:25 -0300

    exemplo de vetor de objetivo v0.4

--- vetobjeto.js
+++ vetobjeto.js
@@ -1,3 +1,12 @@
 function funcao(){
-    
+    let vet = []
+    for(let i=0;i<5;i++)
+        let objeto = {}
+        // coloca dados no objeto
+        objeto.codigo = Number(prompt('Codigo'))
+        objeto.nome = prompt('Nome')
+        objeto.qtde = Number(prompt('Quantidade'))
+        objeto.preco = Number(prompt('Preço'))
+        // coloca objeto no vetor
+        vet.push(objeto)
 }
